86c8699dc185cac8efa4660222fa8b71d5cea706
Euan Mendoza <bebopbamf@effectfree.dev>
2022-02-17 13:58:55 +1100

    sanity codegen support

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  dataset: 'production',
-  projectId: 'v9scvo27',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: '2021-03-25',
   useCdn: process.env.NODE_ENV === 'production',
 };
