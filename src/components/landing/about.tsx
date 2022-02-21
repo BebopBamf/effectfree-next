4296c4c725d168f36ed2438d425c243d17af2a74
Euan Mendoza <bebopbamf@effectfree.dev>
2022-02-22 10:43:19 +1100

    changed about page stuff and removed studio submodule commit info

--- src/components/landing/about.tsx
+++ src/components/landing/about.tsx
@@ -6,6 +6,10 @@ const About = () => (
       <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">
         About
       </h2>
+      <div className="divider-custom divider-light">
+        <div className="divider-custom-line" />
+        <div className="divider-custom-line" />
+      </div>
     </Container>
   </section>
 );
